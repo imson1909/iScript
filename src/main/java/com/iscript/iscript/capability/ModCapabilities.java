e7441f131fe1d0c5b172ea766867709dbe7a0bf6
imson <236419588+imson1909@users.noreply.github.com>
2026-07-07 23:49:29 +0600

    Delete src/main/java/com/iscript/iscript directory

--- src/main/java/com/iscript/iscript/capability/ModCapabilities.java
+++
@@ -1,9 +0,0 @@
-package com.iscript.iscript.capability;
-
-import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.capabilities.CapabilityManager;
-import net.minecraftforge.common.capabilities.CapabilityToken;
-
-public class ModCapabilities {
-    public static final Capability<PlayerQuestData> PLAYER_QUESTS = CapabilityManager.get(new CapabilityToken<>() {});
-}
